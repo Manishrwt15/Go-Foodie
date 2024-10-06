4182ba491d9349cc09bcd938d1a2983efc44da8c
Yamini Joshi <111858583+Yamini-toto@users.noreply.github.com>
2024-10-06 09:14:21 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -5,7 +5,7 @@ import Stripe from 'stripe'
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 //placing user order from fronend
 const placeOrder = async(req,res) => {
-    const frontend_url = "https://go-foodie-frontend.onrender.com/";
+    const frontend_url = "https://go-foodie-frontend.onrender.com";
 
     try{
             const newOrder = new orderModel({
